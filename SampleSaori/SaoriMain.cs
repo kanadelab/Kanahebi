291d609cd077d814bbdc686f64409913c0dc0402
ななっち <nanachi@hotmail.co.jp>
2023-10-21 21:26:50 +0900

    Saoriサンプルを追加

---
+++ SampleSaori/SaoriMain.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleSaori
+{
+	internal static class SaoriMain
+	{
+		/// <summary>
+		/// SAORIのload時に呼ばれる
+		/// </summary>
+		public static void Load()
+		{
+
+		}
+
+		/// <summary>
+		/// Saoriのunload時に呼ばれる
+		/// </summary>
+		public static void Unload()
+		{
+
+		}
+
+		/// <summary>
+		/// Saoriのrequest時に呼ばれる
+		/// </summary>
+		public static string Request(Dictionary<int, string> args, ref string[] values)
+		{
+			var command = args[0];
+			switch (command)
+			{
+				case "Test":
+					return "SampleSaori-Test";
+			}
+			throw new NotImplementedException();
+		}
+	}
+}
